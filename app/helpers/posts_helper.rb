56f0dc5bf45de4b571ef118b792b935bda83d430
Will Larry <will23larry@gmail.com>
2018-03-10 01:03:51 -0500

    Added avatars(Gravatar) to users

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -5,4 +5,10 @@ module PostsHelper
     form.submit("Post!")
     end
   end
+
+  def avatar(user)
+    email_digest = Digest::MD5.hexdigest user.email
+    gravatar_url = "//www.gravatar.com/avatar/#{email_digest}"
+    image_tag gravatar_url
+  end
 end
