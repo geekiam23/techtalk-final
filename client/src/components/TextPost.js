2443525f86e34afbab8719ce4ed4004d4853911f
Will Larry <will23larry@gmail.com>
2018-03-19 11:35:26 -0400

    Changed and styled TextPost Component to show data instead of being an input component

--- client/src/components/TextPost.js
+++ client/src/components/TextPost.js
@@ -1,28 +1,40 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+// import { Button } from 'reactstrap';
 
 export default class TextPost extends React.Component{
-  constructor(props){
-    super(props);
-
-    this.state = {
-      body: this.props.body
-    }
-    this.handleBodyChange = this.handleBodyChange.bind(this);
-  }
-
-  handleBodyChange(e) {
-    this.setState({ body: e.target.value });
-  }
   render (){
+    let indLinks = {
+      position: "relative",
+      display: "inline",
+      width: "50%",
+      float: "both"
+    }
+
+    let divContainerLinks = {
+      bottom: "0",
+      position: "relative"
+    }
+
+    let textBodyAlignment = {
+       'textAlign': "center"
+    }
+
     return (
-      <div>
-        <input
-          type="text"
-          name="post[body]"
-          value={this.state.body}
-          onChange={this.handleBodyChange} placeholder="Post Here!"/>
-          <Button color="danger">Post!</Button>
+      <div className="mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--3dp">
+        <div className="mdl-card__title" style={textBodyAlignment}>
+           <h4 className="mdl-card__title-text">{this.props.body}</h4>
+        </div>
+        <div className="mdl-card__supporting-text">
+          <p className="mdl-typography--font-light mdl-typography--subhead">{this.props.created_at}</p>
+        </div>
+        <div style={divContainerLinks}>
+          <div className="mdl-card__actions" style={indLinks}>
+             <a className="techtalk-link mdl-button mdl-js-button mdl-typography--text-uppercase" href="">
+               Reply
+               <i className="material-icons">chevron_right</i>
+             </a>
+          </div>
+        </div>
       </div>
     )
   }
