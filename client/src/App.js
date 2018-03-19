3758d541e9898eb82cb05601f19e0184806c7d1c
Will Larry <will23larry@gmail.com>
2018-03-19 11:44:11 -0400

    Changed and refactored App.js because I wanted to bring the api into the PostCardGroup instead of App.js. Also added SideBar, Jumbotron, and PostButton components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,78 +1,26 @@
 import React, { Component } from 'react';
-import { Segment } from 'semantic-ui-react';
-import PostCard from './components/PostCard';
+import PostCardGroup from './components/PostCardGroup';
 import MenuBar from './components/MenuBar';
+import SideBar from './components/SideBar';
 import Footer from './components/Footer';
+import JumboTron from './components/JumboTron';
+import PostButton from './components/PostButton';
 
 
 class App extends Component {
-
-  // componentDidMount() {
-  //   this.fetchPosts();
-  // }
-  //
-  //
-  // fetchPosts() {
-  //   fetch('api/posts/1')
-  //     .then((resp) => {
-  //       return resp.json();
-  //     })
-  //     .then((posts) => {
-  //       this.setState([
-  //         posts: posts
-  //       ]);
-  //     });
-  // }
-  //
-  // fetchPost(id) {
-  //   return fetch(`api/posts/${id}`)
-  //     .then((resp) => {
-  //       return resp.json();
-  //     });
-  // }
-  //
-  // handleSetPost(id) {
-  //   this.fetchPost(id)
-  //     .then((post) => {
-  //       this.setState({
-  //         activePost: post
-  //       });
-  //     })
-  // }
-
-  // componentDidMount() {
-  //   window.fetch('api/posts')
-  //     .then(response => response.json())
-  //     .then(json => console.log(json))
-  //     .catch(error => console.log(error))
-  // }
-
   render() {
-    // const postCards = this.state.posts.length ?
-    // this.state.posts.map((post, index) => (
-    //   <PostCard
-    //     key={index}
-    //     post={post}
-    //     handleSetPost={this.handleSetPost}
-    //   />
-    // )) : "";
-    //
-    // const postsGroup = !this.state.activePost ?
-    //   (
-    //     <Card.Group className="post-cards-group">
-    //       {postCards}
-    //     </Card.Group>
-    //   ) : '';
-    //
     return (
       <div className='App'>
-        <MenuBar/>
-          <PostCard></PostCard>
-            <Segment>
-            <p>
-            </p>
-          </Segment>
-        <Footer/>
+        <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+          <div className="techtalk-content mdl-layout__content">
+            <MenuBar/>
+            <SideBar/>
+              <JumboTron/>
+              <PostCardGroup/>
+              <PostButton/>
+            <Footer/>
+          </div>
+        </div>
       </div>
     );
   }
