44710ec77d863829e6cce6e93a35a2212ad30f36
Will Larry <will23larry@gmail.com>
2018-03-20 13:34:31 -0400

    Added userinfo  (avatar, username, and date created) to each postcard.

---
+++ client/src/components/UserInfo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Avatar from 'material-ui/Avatar';
+import FontIcon from 'material-ui/FontIcon';
+import ListItem from 'material-ui/List/ListItem';
+
+const UserInfo = () => (
+  <ListItem
+    disabled={true}
+    leftAvatar=
+      {<Avatar>A</Avatar>}
+    >
+  Image Avatar
+  </ListItem>
+)
+
+export default UserInfo;
