536afad9052b20051715a5dd1d09145b20358af4
Will Larry <will23larry@gmail.com>
2018-03-20 13:15:50 -0400

    If the current user is not signed in, they will be designated as a guest.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
+
+  def current_user
+    super || Guest.new
+  end
 end
