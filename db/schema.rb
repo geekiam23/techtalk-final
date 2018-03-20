1a27e097f46e811cf1cc0645d21283e5eeebbeb8
Will Larry <will23larry@gmail.com>
2018-03-20 13:34:59 -0400

    Guest migration was successful.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312142346) do
+ActiveRecord::Schema.define(version: 20180320165731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20180312142346) do
     t.datetime "updated_at",       null: false
     t.index ["followed_user_id"], name: "index_following_relationships_on_followed_user_id", using: :btree
     t.index ["follower_id"], name: "index_following_relationships_on_follower_id", using: :btree
+  end
+
+  create_table "guests", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "job_posts", force: :cascade do |t|
