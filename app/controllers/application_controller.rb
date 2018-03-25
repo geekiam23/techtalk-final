b61865a3b7c61d473a5b4ae562221195ac5cb394
Will Larry <will23larry@gmail.com>
2018-03-25 18:45:59 -0400

    Added Devise Auth Token (Rails) to App

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  # before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # include DeviseTokenAuth::Concerns::SetUserByToken
+  protect_from_forgery with: :null_session, if: ->{request.format.json?}
+  skip_before_action :verify_authenticity_token
+  respond_to :json
 
-  protected
+  # before_action :underscore_params!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  before_action :authenticate_user, except: [:new, :create]
+
+  private
 
   def configure_permitted_parameters
     added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
@@ -14,4 +20,4 @@ class ApplicationController < ActionController::Base
   def current_user
     super || Guest.new
   end
-end
+ end
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, "137915063365-scj92jsogv0c8b8uoecf8fb40147l064.apps.googleusercontent.com", "daY24yCQ3e_mf7qikgNA9qmt"
+end
