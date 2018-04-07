d26db97040fffade6a39694c69c19d5b4a6c02df
Will Larry <will23larry@gmail.com>
2018-04-06 21:41:54 -0400

    Took out extra stuff in App.js to try and fix a bug

--- client/src/App.js
+++ client/src/App.js
@@ -11,60 +11,60 @@ import $ from 'jquery';
 
 $.auth.configure({
   apiUrl:                '/api',
-  signOutPath:           '/auth/sign_out',
-  emailSignInPath:       '/auth/sign_in',
-  emailRegistrationPath: '/auth',
-  accountUpdatePath:     '/auth',
-  accountDeletePath:     '/auth',
-  passwordResetPath:     '/auth/password',
-  passwordUpdatePath:    '/auth/password',
-  tokenValidationPath:   '/auth/validate_token',
-  proxyIf:               function() { return false; },
-  proxyUrl:              '/proxy',
-  validateOnPageLoad:    false,
-  forceHardRedirect:     false,
-  storage:               'cookies',
-  cookieExpiry:          14,
-  cookiePath:            '/',
+  // signOutPath:           '/auth/sign_out',
+  // emailSignInPath:       'api/auth/sign_in',
+  // emailRegistrationPath: '/auth',
+  // accountUpdatePath:     '/auth',
+  // accountDeletePath:     '/auth',
+  // passwordResetPath:     '/auth/password',
+  // passwordUpdatePath:    '/auth/password',
+  // tokenValidationPath:   '/auth/validate_token',
+  // proxyIf:               function() { return false; },
+  // proxyUrl:              '/proxy',
+  // validateOnPageLoad:    false,
+  // forceHardRedirect:     false,
+  // storage:               'cookies',
+  // cookieExpiry:          14,
+  // cookiePath:            '/',
 
-  passwordResetSuccessUrl: function() {
-    return window.location.href;
-  },
+  // passwordResetSuccessUrl: function() {
+    // return window.location.href;
+  // },
 
-  confirmationSuccessUrl:  function() {
-    return window.location.href;
-  },
+  // confirmationSuccessUrl:  function() {
+    // return window.location.href;
+  // },
 
-  tokenFormat: {
-    "access-token": "{{ access-token }}",
-    "token-type":   "Bearer",
-    client:         "{{ client }}",
-    expiry:         "{{ expiry }}",
-    uid:            "{{ uid }}"
-  },
+  // tokenFormat: {
+    // "access-token": "{{ access-token }}",
+    // "token-type":   "Bearer",
+    // client:         "{{ client }}",
+    // expiry:         "{{ expiry }}",
+    // uid:            "{{ uid }}"
+  // },
 
-  parseExpiry: function(headers){
-    // convert from ruby time (seconds) to js time (millis)
-    return (parseInt(headers['expiry'], 10) * 1000) || null;
-  },
+  // parseExpiry: function(headers){
+    // // convert from ruby time (seconds) to js time (millis)
+    // return (parseInt(headers['expiry'], 10) * 1000) || null;
+  // },
 
-  handleLoginResponse: function(resp) {
-    return resp.data;
-  },
+  // handleLoginResponse: function(resp) {
+    // return resp.data;
+  // },
 
-  handleAccountUpdateResponse: function(resp) {
-    return resp.data;
-  },
+  // handleAccountUpdateResponse: function(resp) {
+    // return resp.data;
+  // },
 
-  handleTokenValidationResponse: function(resp) {
-    return resp.data;
-  },
+  // handleTokenValidationResponse: function(resp) {
+    // return resp.data;
+  // },
 
-  authProviderPaths: {
-    github:    '/auth/github',
-    facebook:  '/auth/facebook',
-    google:    '/auth/google_oauth2'
-  }
+  // authProviderPaths: {
+    // github:    '/auth/github',
+    // facebook:  '/auth/facebook',
+    // google:    '/auth/google_oauth2'
+  // }
 });
 
 class App extends Component {
